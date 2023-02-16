83d61cf6327cab53d3d25136fc7985b2953686b6
maerdlina <maerdlina@gmail.com>
2023-02-16 11:25:48 +0300

    елка исправлена

--- JS/4.js
+++ JS/4.js
@@ -1,15 +1,19 @@
 let kol_vo = Number(prompt('Введите кол-во слоев елки'));
 let stroka1 = '*';
-let stroka2 = '';
 
 document.write(`<p>${stroka1}<p>`);
 for (let i = 0; i < kol_vo; i++){
     if (i % 2 == 0){
-        stroka2 += '##';
-        document.write(`<p>${stroka2}<p>`);
+        stroka1 = stroka1.split('*').join('#');
+        stroka1 += '#';
+        document.write(`<p>${stroka1}<p>`);
     } else{
-        stroka1 += '**';
+        stroka1 = stroka1.split('#').join('*');
+        stroka1 += '*';
         document.write(`<p>${stroka1}<p>`);
     }
 }
-document.write(`<p>||<p>`);
+stroka1 = stroka1.replace(/[^a-zа-яё]/gi, '');
+stroka1 += '||'
+document.write(stroka1);
+
