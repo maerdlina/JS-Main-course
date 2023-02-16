d4f5eb4178fdb748918d9d8898b18b443e869680
maerdlina <maerdlina@gmail.com>
2023-02-16 11:45:43 +0300

    fizz buzz update 3.js

--- JS/3.js
+++ JS/3.js
@@ -3,11 +3,11 @@ let fizz_buzz = Number(prompt('Введите число'));
 
 if (!isNaN(fizz_buzz)){
     for(let i = 0; i <= fizz_buzz; i++){
-        if(i % 2  == 0){
-            document.write(`<p>${i} buzz</p>`)
-        }
-        else if(i % 5 == 0){
+        if(i % 5 == 0){
             document.write(`<p>${i} fizz buzz</p>`)
+        }
+        else if(i % 2  == 0){
+            document.write(`<p>${i} buzz</p>`)
         } else{
             document.write(`<p>${i} fizz</p>`)
         }
