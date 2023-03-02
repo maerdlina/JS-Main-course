166e498783382b7e6fb84c8051da6e48ac8457dc
maerdlina <maerdlina@gmail.com>
2023-03-02 14:44:31 +0300

    3 and 4 upfate

--- JS/2/3.js
+++ JS/2/3.js
@@ -3,8 +3,19 @@ let student = {
     group: 201,
     last_name: "Иванов",
     first_name: "Иван"
-    };
+};
 
-let properties = Object.keys(student).reverse();
 document.write(`<p>Список свойств: ${Object.keys(student)}</p>`)
 document.write(`<p>Студент ${student.first_name} ${student.last_name} учится в ${student.group} группе</p>`)
+
+// let properties = Object.keys(student).reverse();
+// console.log(properties);
+// for(var i = 0; i < properties.length; i++){
+//     if(i == 0){
+//         document.write(`Студент ${student[properties[i]]}`)
+//     } else if(i == 1){
+//         document.write(` ${student[properties[i]]} учится в `)
+//     } else if(i == 2){
+//         document.write(`${student[properties[i]]}группе`)
+//     }
+// }
--- JS/2/4.js
+++ JS/2/4.js
@@ -2,7 +2,6 @@ math_rand = function(min, max){
     document.write(Math.floor(Math.random() * (max - min) + min));
 }
 
-
 let min = Number(prompt('Введите min'));
 let max = Number(prompt('Введите max'));
 
